c1ed8f3258d145764cd75364a40ca1dc56c0d2f1
Peter Drienko <drienkop@gmail.com>
2021-11-24 13:53:14 +0100

    change fastapi metadata

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from src.api.api_v1.api import api_router
 
-app = FastAPI(description='My API')
+app = FastAPI(title='Simple-CRM',
+              version='1.0.0',
+              description='Simple-CRM API is your gateway to the CRM backend.')
 
 app.include_router(api_router, prefix='/api/v1')
